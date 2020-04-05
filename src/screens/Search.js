b276e1fa752c6df8afb0ca548a8b419616c275aa
BilalSattar <ch_aryan2006@yahoo.com>
2020-04-05 23:24:21 +0800

    Hook conversion Search Detail

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -4,13 +4,13 @@ import React from 'react';
 import {Button, View, Text} from 'react-native';
 import {useNavigation} from '@react-navigation/core';
 
-export default function SearchDetail() {
+export default function SearchDetail(props) {
   const navigation = useNavigation();
   const navigateToSearhDetail = () => {
     navigation.navigate('SearchDetail');
     navigation.push('SearchDetail');
   };
-  const {searchId} = this.props.route.params || 'Default';
+  const {searchId} = props.route.params || 'Default';
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
--- src/screens/SearchDetail.js
+++ src/screens/SearchDetail.js
@@ -1,16 +1,12 @@
 // screens/SearchDetails.js
 
-import React, {Component} from 'react';
+import React from 'react';
 import {View, Text} from 'react-native';
 
-class SearchDetails extends Component {
-  render() {
-    return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Search Details screen</Text>
-      </View>
-    );
-  }
+export default function SearchDetail() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Search Details screen</Text>
+    </View>
+  );
 }
-
-export default SearchDetails;
