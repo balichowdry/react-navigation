5e8dc4d04a7dd3a0478d9b9aedb9b5ea458d799b
BilalSattar <ch_aryan2006@yahoo.com>
2020-04-05 18:51:02 +0800

    Updated Structure for container

---
+++ src/navigation/container/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '../../../Screens/Home';
+import Search from '../../../Screens/Search';
+import SearchDetail from '../../../Screens/SearchDetail';
+// import MyDrawer from './src/navigation/drawer/MyDrawer';
+
+const Stack = createStackNavigator();
+
+function NavStack() {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerTitleAlign: 'center',
+                headerStyle: {
+                    backgroundColor: '#621FF7',
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle :{
+                    fontWeight: 'bold',
+                },
+            }}
+        >
+            <Stack.Screen
+                name="Home"
+                component={Home}
+                options={{ title: 'Home' }}
+            />
+            <Stack.Screen
+                name="Search"
+                component={Search}
+                options={{ title: 'Search' }}
+            />
+            <Stack.Screen
+                name="SearchDetail"
+                component={SearchDetail}
+                options={{ title: 'Search Detail' }}
+            />
+        </Stack.Navigator>
+    );
+}
+
+const Container = () => {
+    return (
+        <NavigationContainer>
+            <NavStack />
+        </NavigationContainer>
+    );
+}
+
+export default Container;
+
+
